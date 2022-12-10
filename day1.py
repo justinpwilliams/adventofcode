b55de3bfe3e45d8d015bdee6a39a3a052c961c5a
Justin Williams <justin.p.williams@gmail.com>
2022-12-10 08:08:14 -0800

    day1 solution

---
+++ day1.py
@@ -0,0 +1,27 @@
+scores = {'A X': 3 + 1,
+          'A Y': 6 + 2,
+          'A Z': 0 + 3,
+          'B X': 0 + 1,
+          'B Y': 3 + 2,
+          'B Z': 6 + 3,
+          'C X': 6 + 1,
+          'C Y': 0 + 2,
+          'C Z': 3 + 3}
+
+with open('day1input.txt') as input:
+    print(sum([scores[line.strip('\n')] for line in input]))
+
+scores2 = {'A X': 0 + 3,
+          'A Y': 3 + 1,
+          'A Z': 6 + 2,
+          'B X': 0 + 1,
+          'B Y': 3 + 2,
+          'B Z': 6 + 3,
+          'C X': 0 + 2,
+          'C Y': 3 + 3,
+          'C Z': 6 + 1}
+
+
+with open('day1input.txt') as input:
+    print(sum([scores2[line.strip('\n')] for line in input]))
+
