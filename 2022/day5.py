984843f8e05335b413aa83824ae0c06860c77171
Justin Williams <justin.p.williams@gmail.com>
2022-12-16 17:31:35 -0800

    "day 5 part 1 complete"

--- 2022/day5.py
+++ 2022/day5.py
@@ -23,20 +23,58 @@ def build_stacks(input_file:TextIO):
 
     blank_pattern = re.compile(r'\s+')
     
-    # TODO stop file early, don't go through all lines.
 
     for line in input_file:
+        if line == '\n':
+            print('end of stack')
+            break
         i = 0
         for crate in get_stack_row(line):
             value = crate[0]
             if not blank_pattern.match(value):
                 stacks[i].insert(0, value)
             i += 1
+    return stacks
+
+
+def make_move(move:str, stacks)->None:
+    """
+    Executes a single move given a move strong of the form 
+    'move num from stack to stack'
+    """
+    input_pattern = re.compile('[0-9]+')
+    move_tuple = input_pattern.findall(move)
+    num = int(move_tuple[0])
+    from_stack = int(move_tuple[1])
+    to_stack = int(move_tuple[2])
+
+    # print(stacks[from_stack-1])
+    # print(stacks[to_stack-1])
+    # j = input('Enter to continue.')
+    
+    for i in range(num):
+        stacks[to_stack-1].append(stacks[from_stack-1].pop())
+
+    # print(move)
+    # print(stacks[from_stack-1])
+    # print(stacks[to_stack-1])
+
+
+def make_moves(input_file:TextIO, stacks):
+    for line in input_file:
+        make_move(line, stacks)
+
+
+def get_stack_tops(stacks)->None:
+    message = ''
+    for stack in stacks:
+        message += stack[-1]
     print(stacks)
+    print(message)
 
 
-    pass
-
-with open('day5input.txt') as input:
-    build_stacks(input)
+with open('day5input.txt') as input_file:
+    stacks = build_stacks(input_file)
+    make_moves(input_file, stacks)
+    get_stack_tops(stacks)
 
